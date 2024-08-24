ae7b0d10ba8a7056581ea284c72996f9e63ff1be
beellz <belal20795@gmail.com>
2024-08-24 20:26:05 +0530

    adding status checked

---
+++ get_status/get_status.py
@@ -0,0 +1,22 @@
+
+import requests # type: ignore
+import json
+
+def get_data_from_api(url, params=None):
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
+        print(response.status_code)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+# Example usage:
+api_url = [ "https://jellyfin.beellz.com" , "https://kindle.beellz.com" , "https://torrent.beellz.com/transmission/web/", "https://google.com"]
+
+
+for i in api_url:
+    print(i)
+    data = get_data_from_api(i)
+
+

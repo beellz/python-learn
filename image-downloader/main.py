3c74ad91d643d179abc54dd8f717522be2455407
beellz <belal20795@gmail.com>
2024-08-28 09:15:22 +0530

    url checker with file exists

--- image-downloader/main.py
+++ image-downloader/main.py
@@ -3,35 +3,47 @@
 # use it headless to download the images 
 # loop to download all images
 
-
-# get the links
+# get the links 
 # get the pagination
-# go to each link
-# find out the 8k or 4k link
-# download the image from that link
+# go to each link 
+# find out the 8k or 4k link 
+# download the image from that link 
 
 from playwright.sync_api import sync_playwright
 import time
 
+def check_link_in_file(file_path, link):
+    with open(file_path, 'r') as file:
+        for line in file:
+            if link in line:
+                return True
+    return False
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    page.goto("https://www.uhdpaper.com/search?q=anime+girl&by-date=true&i=1")
+    page.goto("https://www.uhdpaper.com/search?q=anime+girl&by-date=true&i=0")
     print(page.title())
-    # page.get_by_test_id("page_body")
-    # load_all = page.get_by_role(".loadM").click()
-    # load_all = await page.locator("#YourId").click()
-    for i in range(1, 10):
+    for i in range(1, 200):
         load_all = page.get_by_text("Load More ").click()
         time.sleep(2)
         link_locators = page.locator('.blog-posts ').get_by_role('link').all()
-        for _ in link_locators:
-            print(_.get_attribute('href'))
-            file1 = open("myfile.txt", "a")  # append mode
-            file1.write(_.get_attribute('href') + "\n" )
-            file1.close()
-
+        for links in link_locators:
+            get_all = (links.get_attribute('href'))
+            file_path = "myfile.txt"
+            if check_link_in_file(file_path, get_all):
+                # print(f"The link '{get_all}' exists in the file.")
+                continue
+            else:
+                print(f"The link '{get_all}' does not exist in the file.")
+                file1 = open("myfile.txt", "a")  # append mode
+                file1.write(links.get_attribute('href') + "\n" )
+                file1.close()
+            #print(get_all)
+        
 
     time.sleep(5)
     browser.close()
+
+
+

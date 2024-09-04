93c78b91cedaf50e69afeeb078e39300b086103d
beellz <belal20795@gmail.com>
2024-09-04 17:02:35 +0530

    changed the api to omdb and getting media type

--- auto-movie-sorter/movie.py
+++ auto-movie-sorter/movie.py
@@ -4,40 +4,29 @@ import os
 import json
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.getenv("API_KEY_OMDB")
 
 params = {
     # "movie_name":"lord of war",
-    "query":"lord of war",
-    "include_adult":"false",   
-    "language":"en-US",
-    "page":"1",
-    "year":"2005"
+    "t":"Dark",
+    # "y":"2005"
     # "accept":"application/json",
     # "Authorization":"Bearer" + API_KEY
 }
 
-url = "https://api.themoviedb.org/3/search/multi?"
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer " + API_KEY
-}
+url = "http://www.omdbapi.com/?apikey=" + API_KEY
+# headers = {
+#     "accept": "application/json",
+#     "Authorization": "Bearer " + API_KEY
+# }
 
-response = requests.get(url, params=params, headers=headers)
+response = requests.get(url, params=params)
 
 print(response.url)
-# print(response.text)
-# print("hello")
-# print(response.json())
-# print(response["title"])
-#  json_data = response.json()
+print(response.text)
 
 
 json_data = response.json()
-# data = json.load(name)
-# test = data.get("['results'][0]")
 
-print(json_data)
-
-
-
+type = json_data['Type']
+print(type)
